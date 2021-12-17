c7e289027bc42fa71bed0a0e8fb88706c69002cc
francescagiovine <francescagiovine@hotmail.com>
2021-12-17 12:37:00 +0000

    done

--- src/app.js
+++ src/app.js
@@ -5,7 +5,25 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector("#generator").addEventListener("click", () => {
+    document.querySelector("#name").innerHTML = nameGenerator();
+  });
 };
+
+let nameGenerator = () => {
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let dom = [".com", ".net", ".org"];
+  return (
+    randomElement(pronoun) + 
+    randomElement(adj) + 
+    randomElement(noun) + 
+    randomElement(dom)
+    );
+};
+
+function randomElement(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
